3673f6fbdeb4bf15b58ab235e2c02366c1f9b068
ranazraddevops <ranazraddevops@gmail.com>
2022-12-07 19:56:18 +0000

    Updated aks.tf

--- iac/aks.tf
+++ iac/aks.tf
@@ -27,23 +27,23 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-resource "azurerm_key_vault_access_policy" "aks_access_to_kv" {
-  depends_on       = [data.azurerm_kubernetes_cluster.aks]
-  key_vault_id     = data.azurerm_key_vault.tf_kv.id
-  tenant_id        = data.azurerm_client_config.current.tenant_id
-  object_id        = data.azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+// resource "azurerm_key_vault_access_policy" "aks_access_to_kv" {
+//   depends_on       = [data.azurerm_kubernetes_cluster.aks]
+//   key_vault_id     = data.azurerm_key_vault.tf_kv.id
+//   tenant_id        = data.azurerm_client_config.current.tenant_id
+//   object_id        = data.azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
 
-  certificate_permissions = [
-    "Get",
-  ]
+//   certificate_permissions = [
+//     "Get",
+//   ]
 
-  secret_permissions = [
-    "Get",
-  ]
-}
+//   secret_permissions = [
+//     "Get",
+//   ]
+// }
 
 resource "helm_release" "ingress" {
-  depends_on       = [azurerm_key_vault_access_policy.aks_access_to_kv]
+  // depends_on       = [azurerm_key_vault_access_policy.aks_access_to_kv]
   name             = "${local.prefix}-nginx"
   repository       = "https://kubernetes.github.io/ingress-nginx/"
   chart            = "ingress-nginx"
@@ -51,26 +51,26 @@ resource "helm_release" "ingress" {
   create_namespace = true
 }
 
-resource "helm_release" "akv2k8" {
-  depends_on       = [helm_release.ingress]
-  name             = "${local.prefix}-akv2k8"
-  repository       = "https://charts.spvapi.no"
-  chart            = "akv2k8s"
-  namespace        = "akv2k8s"
-  create_namespace = true
-}
+// resource "helm_release" "akv2k8" {
+//   depends_on       = [helm_release.ingress]
+//   name             = "${local.prefix}-akv2k8"
+//   repository       = "https://charts.spvapi.no"
+//   chart            = "akv2k8s"
+//   namespace        = "akv2k8s"
+//   create_namespace = true
+// }
 
-resource "kubectl_manifest" "create_namespace" {
-  depends_on = [helm_release.akv2k8] 
-  yaml_body  = file("../manifests/createNamespace.yaml")
-}
+// resource "kubectl_manifest" "create_namespace" {
+//   depends_on = [helm_release.akv2k8] 
+//   yaml_body  = file("../manifests/createNamespace.yaml")
+// }
 
-resource "kubectl_manifest" "sync_cert_service" {
-  depends_on = [kubectl_manifest.create_namespace] 
-  yaml_body  = file("../manifests/syncCert.yaml")
-}
+// resource "kubectl_manifest" "sync_cert_service" {
+//   depends_on = [kubectl_manifest.create_namespace] 
+//   yaml_body  = file("../manifests/syncCert.yaml")
+// }
 
-resource "kubectl_manifest" "use_cert_service" {
-  depends_on = [kubectl_manifest.sync_cert_service] 
-  yaml_body  = file("../manifests/useCert.yaml")
-}
+// resource "kubectl_manifest" "use_cert_service" {
+//   depends_on = [kubectl_manifest.sync_cert_service] 
+//   yaml_body  = file("../manifests/useCert.yaml")
+// }
