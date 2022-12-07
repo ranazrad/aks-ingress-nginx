432b52dd10e0eed2dd3a96c62ca6b2d0d4c312b7
ranazraddevops <ranazraddevops@gmail.com>
2022-12-07 03:57:39 +0000

    Added versions.tf

---
+++ iac/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "1.2.5"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.15.1"
+    }
+	
+	helm = {
+	  source  = "hashicorp/helm"
+      version = "2.6.0"
+	}	
+	
+	kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+  }
+  
+  backend "azurerm" {}
+}
