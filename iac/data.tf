eb4ff739b98c7236317125f47ca1428933ade6e4
ranazraddevops <ranazraddevops@gmail.com>
2022-12-07 20:21:15 +0000

    Updated data.tf

--- iac/data.tf
+++ iac/data.tf
@@ -1,12 +1,12 @@
-data "azurerm_client_config" "current" {}
+// data "azurerm_client_config" "current" {}
 
-data "azurerm_key_vault" "tf_kv" {
-  name                = azurerm_key_vault.mssre_vault.name
-  resource_group_name = azurerm_key_vault.mssre_vault.name
-}
+// data "azurerm_key_vault" "tf_kv" {
+//   name                = azurerm_key_vault.mssre_vault.name
+//   resource_group_name = azurerm_key_vault.mssre_vault.name
+// }
 
-data "azurerm_kubernetes_cluster" "aks" {
-  depends_on          = [azurerm_kubernetes_cluster.aks]
-  name                = azurerm_kubernetes_cluster.aks.name
-  resource_group_name = azurerm_kubernetes_cluster.aks.resource_group_name
-}
+// data "azurerm_kubernetes_cluster" "aks" {
+//   depends_on          = [azurerm_kubernetes_cluster.aks]
+//   name                = azurerm_kubernetes_cluster.aks.name
+//   resource_group_name = azurerm_kubernetes_cluster.aks.resource_group_name
+// }
