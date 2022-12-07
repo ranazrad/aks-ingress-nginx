0eb6a9ab12878d42f0129cbd482bbbb5e77bf16d
ranazraddevops <ranazraddevops@gmail.com>
2022-12-07 20:21:07 +0000

    Updated kv.tf

--- iac/kv.tf
+++ iac/kv.tf
@@ -1,33 +1,33 @@
-resource "azurerm_resource_group" "mssre_vault_rg" {
-  name     = "mssre-vault"
-  location = "East US"
-}
+// resource "azurerm_resource_group" "mssre_vault_rg" {
+//   name     = "mssre-vault"
+//   location = "East US"
+// }
 
-resource "azurerm_key_vault" "mssre_vault" {
-  name                        = "mssre-vault"
-  location                    = azurerm_resource_group.mssre_vault_rg.location
-  resource_group_name         = azurerm_resource_group.mssre_vault_rg.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+// resource "azurerm_key_vault" "mssre_vault" {
+//   name                        = "mssre-vault"
+//   location                    = azurerm_resource_group.mssre_vault_rg.location
+//   resource_group_name         = azurerm_resource_group.mssre_vault_rg.name
+//   enabled_for_disk_encryption = true
+//   tenant_id                   = data.azurerm_client_config.current.tenant_id
+//   soft_delete_retention_days  = 7
+//   purge_protection_enabled    = false
 
-  sku_name = "standard"
+//   sku_name = "standard"
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+//   access_policy {
+//     tenant_id = data.azurerm_client_config.current.tenant_id
+//     object_id = data.azurerm_client_config.current.object_id
 
-    key_permissions = [
-      "Get",
-    ]
+//     key_permissions = [
+//       "Get",
+//     ]
 
-    secret_permissions = [
-      "Get",
-    ]
+//     secret_permissions = [
+//       "Get",
+//     ]
 
-    storage_permissions = [
-      "Get",
-    ]
-  }
-}
+//     storage_permissions = [
+//       "Get",
+//     ]
+//   }
+// }
