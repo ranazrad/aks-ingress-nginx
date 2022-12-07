d54f255bd8aeed0299aaa8101373de9a45b55d3e
ranazraddevops <ranazraddevops@gmail.com>
2022-12-07 21:28:37 +0000

    Updated providers.tf

--- iac/providers.tf
+++ iac/providers.tf
@@ -13,10 +13,10 @@ provider "helm" {
   }
 }
 
-// provider "kubectl" {
-//   host                   = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
-//   client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-//   client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-//   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-//   load_config_file       = false
-// }
+provider "kubectl" {
+  host                   = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+  load_config_file       = false
+}
